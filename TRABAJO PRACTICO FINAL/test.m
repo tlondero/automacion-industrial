e5ccf9a01d3014bb82e15a5d4bd902bda7e864dd
tlondero <tlondero@itba.edu.ar>
2022-02-27 18:04:13 -0300

    Update test.m

--- TRABAJO PRACTICO FINAL/test.m
+++ TRABAJO PRACTICO FINAL/test.m
@@ -16,23 +16,24 @@ L(5) = Link([0 0 L5 0]);
 Widow = SerialLink(L);
 Widow.name = 'WidowXMKII';
 
-x0 = Lp + L4;
-y0 = L3;
-z0 = L1;    % Altura de la mesa
+% Parametros de la hoja
+w_hoja = 0.15;
+l_hoja = 0.2;
+x_hoja = 0.3;
+y_hoja = -0.1;
+z_hoja = 0;
 
-R = [ 0, 1, 0; 0, 0, -1; -1, 0, 0];
-P = [x0; y0; z0]; 
+x0 = 0.4;
+y0 = 0;
+z0 = 0.2;
+R = [1, 0, 0; 0, 0, -1; 0, 1, 0];
+P = [x0; y0; z0]; %[x0; y0; z0]; 
 T = [R, P; 0, 0, 0, 1];
 
 hold on
-width = 0.15;
-length = 0.2;
-x0 = 0.3;
-y0 = -0.1;
-z0 = 0;
 
-drawTable(width, length, x0, y0, z0);
-Q = Widow.ikine(T, 'mask', [1 1 1 0 0 0]);
+drawTable(w_hoja, l_hoja, x_hoja, y_hoja, z_hoja);
+Q = Widow.ikine(T, 'mask', [1 1 1 1 0 1]);
 Widow.plot(Q);
 zlim([-0.6, Lp+L4+L1]);
 
