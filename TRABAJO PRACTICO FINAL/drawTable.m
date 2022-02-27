985734c704fa54741e5ef235bd7743b1e1ebf6b2
tlondero <tlondero@itba.edu.ar>
2022-02-27 17:51:11 -0300

    Final Draw Table

--- TRABAJO PRACTICO FINAL/drawTable.m
+++ TRABAJO PRACTICO FINAL/drawTable.m
@@ -2,24 +2,18 @@ function drawTable(width,length,x0,y0,z0,color)
 % DRAWTABLE Dibuja los cuatro lados de la mesa en el dibujo 3D
 %     drawTable(width,length,x0,y0,color) width y length son el largo y
 %     ancho de la mesa. X0, Y0 y Z0 es la coordenada inferior izquierda de la
-%     mesa en el plano y la altura. Color: opcional, son las mismas opciones que posee
-%     la funcion plot().
+%     mesa en el plano y la altura. Color: opcional, array de RGB double.
 
     if ~exist('color')
-        color = 'blue';
+        color = [0, 0, 1];
     end
+	
+	X = [x0, x0 + width, x0 + width, x0];
+	Y = [y0, y0, y0 + length, y0 + length];
+	Z = [z0, z0, z0, z0];
     
-    x = linspace(x0,x0+width,1000);
-	y = y0 + x.*0;
-	plot3(x,y,z0 + x.*0,color);
-
-	y = y0 + length + x.*0;
-	plot3(x,y,z0 + x.*0,color);
-
-	y = linspace(y0,y0 + length,1000);
-	x = x0 + y.*0;
-	plot3(x,y,z0 + y.*0,color);
-
-	x = x0 + width + y.*0;
-	plot3(x,y,z0 + y.*0,color);    
+    h = fill3(X,Y,Z,[0, 0, 1]);
+    h.FaceColor = color;
+    h.FaceAlpha = 0.3;
+    
 end
--- TRABAJO PRACTICO FINAL/test.m
+++ TRABAJO PRACTICO FINAL/test.m
@@ -16,20 +16,24 @@ L(5) = Link([0 0 L5 0]);
 Widow = SerialLink(L);
 Widow.name = 'WidowXMKII';
 
-x0 = Lp+L4+L5;
-y0 = 0;
+x0 = Lp + L4;
+y0 = L3;
 z0 = L1;    % Altura de la mesa
 
-R = [ 0, 1, 0;
-      0, 0, -1;
-     -1, 0, 0];
+R = [ 0, 1, 0; 0, 0, -1; -1, 0, 0];
 P = [x0; y0; z0]; 
 T = [R, P; 0, 0, 0, 1];
 
 hold on
-drawTable(0.15,0.2,0.3,-0.1,0.2);
+width = 0.15;
+length = 0.2;
+x0 = 0.3;
+y0 = -0.1;
+z0 = 0;
 
+drawTable(width, length, x0, y0, z0);
 Q = Widow.ikine(T, 'mask', [1 1 1 0 0 0]);
 Widow.plot(Q);
+zlim([-0.6, Lp+L4+L1]);
 
 hold off
