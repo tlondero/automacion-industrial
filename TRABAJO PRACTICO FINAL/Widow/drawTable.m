222d4b899a2e4e41cf4512c92efa94c271c358d9
tlondero <tlondero@itba.edu.ar>
2022-02-28 11:57:24 -0300

    Así como está anda lindo bonito simpatico simetrico

--- TRABAJO PRACTICO FINAL/Widow/drawTable.m
+++ TRABAJO PRACTICO FINAL/Widow/drawTable.m
@@ -1,7 +1,7 @@
 function drawTable(width,length,x0,y0,z0,color)
 % DRAWTABLE Dibuja los cuatro lados de la mesa en el dibujo 3D
 %     drawTable(width,length,x0,y0,color) width y length son el largo y
-%     ancho de la mesa. X0, Y0 y Z0 es la coordenada inferior izquierda de la
+%     ancho de la mesa. X0, Y0 y Z0 es la coordenada superior izquierda de la
 %     mesa en el plano y la altura. Color: opcional, array de RGB double.
 
     if ~exist('color')
@@ -9,7 +9,7 @@ function drawTable(width,length,x0,y0,z0,color)
     end
 	
 	X = [x0, x0 + width, x0 + width, x0];
-	Y = [y0, y0, y0 + length, y0 + length];
+	Y = [y0, y0, y0 - length, y0 - length];
 	Z = [z0, z0, z0, z0];
     
     h = fill3(X,Y,Z,[0, 0, 1]);
