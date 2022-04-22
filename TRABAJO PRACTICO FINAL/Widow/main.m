575a92e4517c174f1355809f81abb5c2e24bd7a3
Guido <glambertucci@itba.edu.ar>
2022-04-22 18:30:42 -0300

    Leer descripcion
    %Hay que actualizar el titulo del "filtor sin limpiado" cuando se da el procesar a procesado
    %ademas que no aparezcan las otras opciones si no se proceso
    %y cuando se le da a procesar que se le haga un update al marco

--- TRABAJO PRACTICO FINAL/Widow/main.m
+++ TRABAJO PRACTICO FINAL/Widow/main.m
@@ -1,3 +1,6 @@
 clc; clear all;close all
 disp('Welcome to CAGT')
 Automacion_GUI
+%Hay que actualizar el titulo del "filtor sin limpiado" cuando se da el procesar a procesado
+%ademas que no aparezcan las otras opciones si no se proceso 
+%y cuando se le da a procesar que se le haga un update al marco
