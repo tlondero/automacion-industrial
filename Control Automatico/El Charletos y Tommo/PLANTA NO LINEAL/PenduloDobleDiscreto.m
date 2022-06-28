bebac7eb0165fe5f672f7899fccb793ecaed0f32
tlondero <tlondero@itba.edu.ar>
2022-06-28 13:27:02 -0300

    @carmaselli estamos "igual" que antes solo que ahora no usamos la planta linealizada

---
+++ Control Automatico/El Charletos y Tommo/PLANTA NO LINEAL/PenduloDobleDiscreto.m
@@ -0,0 +1,92 @@
+clear all; clc;
+
+simulation = 'DoublePendulumDiscreto';
+
+if ~bdIsLoaded(simulation)   % Abro SimuLink si no está abierto
+    open_system(simulation)
+end
+
+%Constantes de simulink
+
+M = 1.5;
+g = 9.8;
+b = 0;
+l = 0.15;
+
+L1 = 0.5;
+L2 = 0.75;
+l1 = L1/2;
+l2 = L2/2;
+m1 = 0.5;
+m2 = 0.75;
+
+I1 = (m1*L1^2)/12;
+I2 = (m2*L2^2)/12;
+
+syms t1 t2;
+
+D = [ M+m1+m2,                  (m1*l1+m2*L1)*cos(t1),  m2*l2*cos(t2);
+      (m1*l1+m2*L1)*cos(t1),    m1*(l1^2)+m2*(L1^2)+I1, m2*L1*l2*cos(t1-t2);
+      m2*l2*cos(t2),            m2*L1*l2*cos(t1-t2),    m2*(l2^2)+I2];
+  
+G = [ 0;
+      -g*(m1*l1+m2*L1)*sin(t1);
+      -m2*l2*g*sin(t2)];
+  
+H = [1;0;0];
+  
+D0 = double(subs(D, {'t1' 't2'}, {0 0}));
+jacobian_g0 = double(subs(jacobian(G, [t1 t2]), {'t1' 't2'}, {0 0}));
+
+A = [ zeros(3,3), eye(3);
+    [ zeros(3,1), -1*(D0\jacobian_g0), zeros(3)] ]
+
+B = [ 0;
+      0;
+      0;
+      D0\H ]
+
+C = [1 0 0 0 0 0];
+
+D = 0;
+
+%% Transferencias Pendulo Doble
+
+% x t1 t2 x_d t1_d t2_d
+
+[num, den] = ss2tf(A,B,[1 0 0 0 0 0],0); Tx = zpk(minreal(tf(num,den)))
+[num, den] = ss2tf(A,B,[0 1 0 0 0 0],0); Tt1 = zpk(minreal(tf(num,den)))
+[num, den] = ss2tf(A,B,[0 0 1 0 0 0],0); Tt2 = zpk(minreal(tf(num,den)))
+
+%% Estabilidad, controlabilidad y observabilidad Pendulo Doble
+
+disp('Estabildiad pendulo simple:')        %Es inestable, tengo un polo en SPD
+eig(A)
+
+disp(['Controlabilidad pendulo simple: ' num2str(rank(ctrb(A,B)))])    %Es controlable
+disp(['Observabilidad pendulo simple: ' num2str(rank(obsv(A,C)))])    %Se puede estimar las variables de estado observando la posición
+
+%% Realimentacion de estados y observador DISCRETO
+
+Ts = 1e-3;
+
+X0 = [0 5*pi/180 5*pi/180 0 0 0];
+
+sysd = c2d(ss(A,B,C,0), Ts, 'tustin');
+Ad = sysd.A;
+Bd = sysd.B;
+Cd = sysd.C;
+Dd = sysd.D;
+
+pK = [-40 -8 -8 -1 -1 -0.5];
+pKd = exp(pK.*Ts);
+Kd = acker(Ad, Bd, pKd)
+
+pL = [-400 -80 -0.001 -10 -10 -0.1];
+% pL = [-400 -80 -0.01 -10 -10 -0.01];
+% pL = [-200 -40 -0.1 -5 -5 -0.05];
+pLd = exp(pL.*Ts);
+Ld = acker(Ad', Cd', pLd)
+Ld = Ld';
+
+sim(simulation,10);
